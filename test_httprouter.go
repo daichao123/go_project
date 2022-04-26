931ea9fa14487b309a985c1a7cd8ea0c81312662
1078638034@qq.com <1078638034@qq.com>
2022-04-26 10:30:52 +0800

    gin first

--- test_httprouter.go
+++ test_httprouter.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
 )
+
+func testGin(ctx *gin.Context) {
+	name := ctx.Query("name")
+	password := ctx.DefaultQuery("password", "test_password")
+	if password != "" && name != "" {
+		ctx.JSON(200, map[string]interface{}{"name:": name, "password:": password})
+	}
+}
 
 func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "test httpRouter")
@@ -16,11 +25,17 @@ func hello(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 }
 
 func main() {
-	router := httprouter.New()
-	router.GET("/", index)
-	router.GET("/echo/:name", hello)
-	err := http.ListenAndServe(":8080", router)
+	engine := gin.Default()
+	engine.GET("/test", testGin)
+	err := engine.Run(":8888")
 	if err != nil {
 		log.Fatal(err)
 	}
+	//router := httprouter.New()
+	//router.GET("/", index)
+	//router.GET("/echo/:name", hello)
+	//err := http.ListenAndServe(":8080", router)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 }
