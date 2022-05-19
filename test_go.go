4e065a0cdb66a49325ef21910ed06bfab0b94086
1078638034@qq.com <1078638034@qq.com>
2022-05-19 15:19:56 +0800

    .

--- test_go.go
+++ test_go.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"reflect"
 )
 
 func NotRepeat(str string) int {
@@ -28,34 +28,81 @@ func IsListDuplicated(list []string) bool {
 	return false
 }
 
+type Cat struct {
+	Name string
+	Type string
+	Tag  string
+}
+
+type Car struct {
+	Name  string
+	Price float64
+	Tag   string
+}
+
 func main() {
-	NotRepeat("abcabcdbb")
-	var computerFist, userFist string
-	computerNum := rand.New(rand.NewSource(123)).Intn(3)
-	switch computerNum {
-	case 0:
-		computerFist = "石头"
-	case 1:
-		computerFist = "石头"
-	case 2:
-		computerFist = "布"
-	}
-	fmt.Println("请出招:")
-	fmt.Scan(&userFist)
-
-	switch userFist {
-	case "石头":
-		if computerFist == "石头" {
-			fmt.Println()
-		} else if computerFist == "布" {
-
-		} else if computerFist == "剪刀" {
-
+	goods := make([]interface{}, 0)
+	goods = append(goods, Cat{Name: "cat test", Type: "type test", Tag: "tag test"})
+	goods = append(goods, Car{Name: "car test", Price: 2300, Tag: "car tag test"})
+	fmt.Printf("goods : %s", goods)
+	for _, item := range goods {
+		//fmt.Print(reflect.ValueOf(item))
+		s := reflect.ValueOf(&item)
+		typeOfT := s.Type()
+		fmt.Printf("typeOFT:%s", typeOfT)
+		for i := 0; i < s.NumField(); i++ {
+			f := s.Field(i)
+			fmt.Printf("%d: %s %s = %v\n", i,
+				typeOfT.Field(i).Name, f.Type(), f.Interface())
 		}
-	case "剪刀":
-	case "布":
-	default:
-		fmt.Println("正确的招式")
+		//fmt.Printf(itemValue.Type())
+		//name := itemValue.FieldByName("Name")
+		//fmt.Print(name.Interface().(string))
+		//fmt.Printf(item.(string))
+		//fmt.Println("item", item.Name)
 	}
+
+	//NotRepeat("abcabcdbb")
+	//var computerFist, userFist string
+	//computerNum := rand.New(rand.NewSource(123)).Intn(3)
+	//switch computerNum {
+	//case 0:
+	//	computerFist = "石头"
+	//case 1:
+	//	computerFist = "石头"
+	//case 2:
+	//	computerFist = "布"
+	//}
+	//fmt.Println("请出招:")
+	//fmt.Scan(&userFist)
+	//
+	//switch userFist {
+	//case "石头":
+	//	if computerFist == "石头" {
+	//		fmt.Println()
+	//	} else if computerFist == "布" {
+	//
+	//	} else if computerFist == "剪刀" {
+	//
+	//	}
+	//case "剪刀":
+	//	if computerFist == "石头" {
+	//		fmt.Println()
+	//	} else if computerFist == "布" {
+	//
+	//	} else if computerFist == "剪刀" {
+	//
+	//	}
+	//case "布":
+	//	if computerFist == "石头" {
+	//		fmt.Println()
+	//	} else if computerFist == "布" {
+	//
+	//	} else if computerFist == "剪刀" {
+	//
+	//	}
+	//default:
+	//	fmt.Println("正确的招式")
+	//}
 
 }
